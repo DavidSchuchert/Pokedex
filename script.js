24ab98871b3b376a56a4a76a19cbbd0bef79ff7b
DavidSchuchert <Davidschuchert@icloud.com>
2023-08-30 11:56:20 +0200

    changed open pokemoncard style

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function loadAllPokemonTypes() {
 function changePokemonCardBackground(i) {
 
     if (allPokemonTypes[i] == "grass") {
-        backgroundColorCards = 'style="background-color: lightgreen"';
+        backgroundColorCards = 'style="background-color: #4cc94c"';
     }
 
     if (allPokemonTypes[i] == "fire") {
@@ -70,112 +70,123 @@ function openPokemonCard(i) {
 
 }
 
+function createChart(i) {
+    const ctx = document.getElementById('myChart');
+
+    let hp = currentPokemon[i]['stats']['0']['base_stat'];
+    let attack = currentPokemon[i]['stats']['1']['base_stat'];
+    let defense = currentPokemon[i]['stats']['2']['base_stat'];
+    let specialattack = currentPokemon[i]['stats']['3']['base_stat'];
+    let specialdefense = currentPokemon[i]['stats']['4']['base_stat'];
+    let speed = currentPokemon[i]['stats']['5']['base_stat'];
+
+
+
+
+
+    new Chart(ctx, {
+        type: 'radar',
+        data: {
+            labels: ['hp', 'attack', 'defense', 'special-attack', 'special-defense', 'speed'],
+            datasets: [{
+                label: 'Base stats',
+                data: [hp, attack, defense, specialattack, specialdefense, speed],
+                backgroundColor: 'rgba(255, 0, 0, 0.52)', // Hintergrundfarbe des Charts
+                borderColor: 'rgba(101, 15, 181, 0.3)', // Farbe der Linie
+                borderWidth: 0.5, // Dicke der Linie
+            }]
+        },
+        options: {
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            scales: {
+                r: {
+                    angleLines: {
+                        display: false
+                    },
+                    suggestedMin: 1,
+                    pointLabels: {
+                        font: {
+                            size: 10,
+                            weight: 700
+                        }
+                    },
+                    ticks: {
+                        display: false
+                    }
+                }
+            }
+        }
+    });
+}
+
 function renderPokemoncardWithMultipleTypes(i) {
+    
     document.getElementById('fullScreenCard').style.display = "flex";
     document.getElementById('fullScreenCard').innerHTML = `
     <div id="fullscreenCardSmall" class="fullscreen_card_small" ${backgroundColorCards}>
-    <h2>${currentPokemon[i]['name']}</h2>
-    <img src="${currentPokemon[i]['sprites']['other']['official-artwork']['front_default']}" alt="">
+    <div class="pkmnNameAndImg"> <h2>${currentPokemon[i]['name']}</h2>
+    <img src="${currentPokemon[i]['sprites']['other']['official-artwork']['front_default']}" alt=""></div>
     <div class="line"></div>
-    <div class="card_buttons">
-        <button onclick="showType(event)" type="button" class="btn btn-primary">Type</button>
-        <button onclick="showStats(event)" type="button" class="btn btn-primary">Stats</button>
-        <button onclick="showAbilitys(event)" type="button" class="btn btn-primary">Start Abilitys</button>
-    </div>
-
+    <div class="pkmninfo">
     <div id="typesection" class="types_section">
-        <h2> Type/s: </h2>
         <div class="show_types" id="showTypes">
 
-            <button type="button" class="btn btn-success">${currentPokemon[i]['types']['0']['type']['name']}</button>
-            <button type="button" class="btn btn-success">${currentPokemon[i]['types']['1']['type']['name']}</button>
+            Types: <p>${currentPokemon[i]['types']['0']['type']['name']} & ${currentPokemon[i]['types']['1']['type']['name']}</p>
         </div>
     </div>
     <div id="abilitysection" class="abilitys_section">
-        <h2> Abilities: </h2>
         <div class="show_abilitys" id="showTypes">
-            <button type="button" class="btn btn-success">${currentPokemon[i]['abilities']['0']['ability']['name']}</button>
-            <button type="button" class="btn btn-success">${currentPokemon[i]['abilities']['1']['ability']['name']}</button>
+            Start Abilities: <p>${currentPokemon[i]['abilities']['0']['ability']['name']} & ${currentPokemon[i]['abilities']['1']['ability']['name']}</p>
         </div>
     </div>
 
     <div id="statssection" class="stats_section">
-        <h2> Stats: </h2>
         <div class="show_stats" id="showTypes">
-            <button type="button" class="btn btn-success">Base Stat: ${currentPokemon[i]['stats']['0']['base_stat']}</button>
+Base Stats:
+        <canvas id="myChart"></canvas>
         </div>
     </div>
-
+</div>
     </div>
     `
-
-    showType(event);
+    createChart(i);
 }
 
 function renderPokemoncardWithOneType(i) {
     document.getElementById('fullScreenCard').style.display = "flex";
     document.getElementById('fullScreenCard').innerHTML = `
     <div id="fullscreenCardSmall" class="fullscreen_card_small" ${backgroundColorCards}>
-    <h2>${currentPokemon[i]['name']}</h2>
-    <img src="${currentPokemon[i]['sprites']['other']['official-artwork']['front_default']}" alt="">
+    <div class="pkmnNameAndImg"> <h2>${currentPokemon[i]['name']}</h2>
+    <img src="${currentPokemon[i]['sprites']['other']['official-artwork']['front_default']}" alt=""></div>
     <div class="line"></div>
-    <div class="card_buttons">
-        <button onclick="showType(event)" type="button" class="btn btn-primary">Type</button>
-        <button onclick="showStats(event)" type="button" class="btn btn-primary">Stats</button>
-        <button onclick="showAbilitys(event)" type="button" class="btn btn-primary">Start Abilitys</button>
-    </div>
-
+    <div class="pkmninfo">
     <div id="typesection" class="types_section">
-        <h2> Type/s: </h2>
         <div class="show_types" id="showTypes">
-            <button type="button" class="btn btn-success">${currentPokemon[i]['types']['0']['type']['name']}</button>
+
+            Types: <p>${currentPokemon[i]['types']['0']['type']['name']}</p>
         </div>
     </div>
-
-
     <div id="abilitysection" class="abilitys_section">
-        <h2> Abilities: </h2>
         <div class="show_abilitys" id="showTypes">
-        <button type="button" class="btn btn-success">${currentPokemon[i]['abilities']['0']['ability']['name']}</button>
-        <button type="button" class="btn btn-success">${currentPokemon[i]['abilities']['1']['ability']['name']}</button>
+            Start Abilities: <p>${currentPokemon[i]['abilities']['0']['ability']['name']} & ${currentPokemon[i]['abilities']['1']['ability']['name']}</p>
         </div>
     </div>
-
 
     <div id="statssection" class="stats_section">
-        <h2> Stats: </h2>
         <div class="show_stats" id="showTypes">
-        <button type="button" class="btn btn-success">Base Stat: ${currentPokemon[i]['stats']['0']['base_stat']}</button>
+Base Stats:
+        <canvas id="myChart"></canvas>
         </div>
     </div>
-  </div>
-`
-
-showType(event);
-
+</div>
+    </div>
+    `
+    createChart(i);
 }
-
-function showStats(event) {
-    document.getElementById('statssection').style.display = "block";
-    document.getElementById('typesection').style.display = "none";
-    document.getElementById('abilitysection').style.display = "none";
-    event.stopPropagation();
-}
-
-function showType(event) {
-    document.getElementById('statssection').style.display = "none";
-    document.getElementById('typesection').style.display = "block";
-    document.getElementById('abilitysection').style.display = "none";
-    event.stopPropagation();
-}
-
-function showAbilitys(event) {
-    document.getElementById('statssection').style.display = "none";
-    document.getElementById('typesection').style.display = "none";
-    document.getElementById('abilitysection').style.display = "block";
-    event.stopPropagation();
-}
-
 
 function closePokemonCard() {
     document.getElementById('fullScreenCard').style.display = "none";
@@ -227,9 +238,9 @@ function renderPokemonInfo(i) {
 
 function addGrassType(i) {
     document.getElementById('bodyMainCards').innerHTML += `
-    <div onclick="openPokemonCard(${(i)})" class="main_card" id="main_card" style="background-color: lightgreen">
+    <div onclick="openPokemonCard(${(i)})" class="main_card" id="main_card" style="background-color: #4cc94c">
     <div class="pokemon_card" id="pokemonCard">
-    <h2 id="PokemonName">${currentPokemon[i]['name']}</h2>
+    <h2 id="PokemonName"> #${[i + 1]}  ${currentPokemon[i]['name']}</h2>
     <img class="pokemon_img" id="pokemon_img" src="${currentPokemon[i]['sprites']['other']['official-artwork']['front_default']}" alt=""> 
     </div>
     </div>
@@ -240,7 +251,7 @@ function addFireType(i) {
     document.getElementById('bodyMainCards').innerHTML += `
     <div  onclick="openPokemonCard(${(i)})" class="main_card" id="main_card" style="background-color: #F3B34C">
     <div class="pokemon_card" id="pokemonCard">
-    <h2 id="PokemonName">${currentPokemon[i]['name']}</h2>
+    <h2 id="PokemonName"> #${[i + 1]}  ${currentPokemon[i]['name']}</h2>
     <img class="pokemon_img" id="pokemon_img" src="${currentPokemon[i]['sprites']['other']['official-artwork']['front_default']}" alt=""> 
     </div>
     </div>
@@ -251,7 +262,7 @@ function addWaterType(i) {
     document.getElementById('bodyMainCards').innerHTML += `
     <div  onclick="openPokemonCard(${(i)})" class="main_card" id="main_card" style="background-color: lightblue">
     <div class="pokemon_card" id="pokemonCard">
-    <h2 id="PokemonName">${currentPokemon[i]['name']}</h2>
+    <h2 id="PokemonName"> #${[i + 1]}  ${currentPokemon[i]['name']}</h2>
     <img class="pokemon_img" id="pokemon_img" src="${currentPokemon[i]['sprites']['other']['official-artwork']['front_default']}" alt=""> 
     </div>
     </div>
@@ -262,7 +273,7 @@ function addBugType(i) {
     document.getElementById('bodyMainCards').innerHTML += `
     <div  onclick="openPokemonCard(${(i)})" class="main_card" id="main_card" style="background-color: lightgreen">
     <div class="pokemon_card" id="pokemonCard">
-    <h2 id="PokemonName">${currentPokemon[i]['name']}</h2>
+    <h2 id="PokemonName"> #${[i + 1]}  ${currentPokemon[i]['name']}</h2>
     <img class="pokemon_img" id="pokemon_img" src="${currentPokemon[i]['sprites']['other']['official-artwork']['front_default']}" alt=""> 
     </div>
     </div>
@@ -273,7 +284,7 @@ function addNormalType(i) {
     document.getElementById('bodyMainCards').innerHTML += `
     <div  onclick="openPokemonCard(${(i)})" class="main_card" id="main_card" style="background-color: gray">
     <div class="pokemon_card" id="pokemonCard">
-    <h2 id="PokemonName">${currentPokemon[i]['name']}</h2>
+    <h2 id="PokemonName"> #${[i + 1]}  ${currentPokemon[i]['name']}</h2>
     <img class="pokemon_img" id="pokemon_img" src="${currentPokemon[i]['sprites']['other']['official-artwork']['front_default']}" alt=""> 
     </div>
     </div>
